19aecb8409731bfb37f12f103f6d17e3ac11b9ba
Louis SINGER <20170936@B15-13.local>
2019-04-03 17:40:27 +0200

    change game implementation

--- Sources/swift-mastermind/main.swift
+++ Sources/swift-mastermind/main.swift
@@ -1,2 +1,54 @@
+import Foundation
+
 let game = Mastermind()
-game.playGame()
+
+func play(theGame game: Mastermind) {
+        var turnNumber = 0
+        game.playing = true
+        var playerLine : Line
+        var result : Array<Int>
+
+        let startDate = Date()
+
+        while(turnNumber < 12 && game.playing) {
+            // --- Enter a line (A REMPLACER) 
+            playerLine = Line.randomLine()
+            // ---
+
+            result = playerLine.result(compareTo: game.code)
+
+            // --- Print the line and his result (A REMPLACER)
+            print("player plays: \(playerLine) | \(result)")
+            // ---
+
+            if result[0] == 4 {
+                game.playing = false
+                game.playerWin = true
+            }
+            else {
+                turnNumber += 1
+            }
+            game.lines.append(playerLine)
+            game.results.append(result)
+        }
+        if game.playing {game.playing = false}
+        let elapsed = Date().timeIntervalSince(startDate)
+        game.elapsedTime = elapsed
+        print("duration: \(elapsed)")
+        
+
+        if game.playerWin {
+            // --- END OF GAME, the player won : display it (A REMPLACER)
+                print("The player wins ! Good game !")
+            // ---
+            game.score = (12 - turnNumber)*10 + Int(elapsed/100)
+            print("score = \(game.score)")
+        } else {
+            // --- END OF GAME, the player lost : display it (A REMPLACER)
+                print("The player lost ! Maybe another time !")
+            // ---
+        }
+
+    }
+
+play(theGame: game)
--- Sources/swift-mastermind/mastermind.swift
+++ Sources/swift-mastermind/mastermind.swift
@@ -8,6 +8,7 @@ class Mastermind {
     var playerWin = false
     var lines : Array<Line> = []
     var results : Array<Array<Int>> = []
+    var elapsedTime: TimeInterval? = nil
 
     init() {
         self.code = Line.randomLine()
@@ -17,53 +18,7 @@ class Mastermind {
         self.code = theCode
     }
 
-    func playGame() {
-        var turnNumber = 0
-        self.playing = true
-        var playerLine : Line
-        var result : Array<Int>
-
-        let startDate = Date()
-
-        while(turnNumber < 12 && self.playing) {
-            // --- Enter a line (A REMPLACER) 
-            playerLine = Line.randomLine()
-            // ---
-
-            result = playerLine.result(compareTo: self.code)
-
-            // --- Print the line and his result (A REMPLACER)
-            print("player plays: \(playerLine) | \(result)")
-            // ---
-
-            if result[0] == 4 {
-                self.playing = false
-                self.playerWin = true
-            }
-            else {
-                turnNumber += 1
-            }
-            self.lines.append(playerLine)
-            self.results.append(result)
-        }
-        if self.playing {self.playing = false}
-        var elapsed = Date().timeIntervalSince(startDate)
-        print("duration: \(elapsed)")
-        
-
-        if self.playerWin {
-            // --- END OF GAME, the player won : display it (A REMPLACER)
-                print("The player wins ! Good game !")
-            // ---
-            self.score = (12 - turnNumber)*10 + Int(elapsed/100)
-            print("score = \(self.score)")
-        } else {
-            // --- END OF GAME, the player lost : display it (A REMPLACER)
-                print("The player lost ! Maybe another time !")
-            // ---
-        }
-
-    }
+    
 
     
 }
