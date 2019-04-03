e8c3d5b8e1429448a6f96c8773b585dcede7563a
Louis SINGER <20170936@B15-13.local>
2019-04-03 15:00:51 +0200

    result: done

--- Sources/swift-mastermind/line.swift
+++ Sources/swift-mastermind/line.swift
@@ -23,8 +23,40 @@ class Line: CustomStringConvertible {
     {
         return [Color.black, Color.black, Color.black, Color.black]
     }
-
+    
     var description: String {
         return "\(colors[0]) - \(colors[1]) - \(colors[2]) - \(colors[3])"
     }
+
+    func wellPlaced(compareTo refline: Line) -> Array<Int>
+    {
+        var result = [Int]()
+        for i in 0...3 {
+            if refline.colors[i] == self.colors[i] {
+                result.append(i)
+            }
+        }
+        return result
+    }
+
+    func wrongPositions(compareTo refline: Line) -> Array<Int>
+    {
+        var result = [Int]()
+        var reflineColorsCopy = refline.colors
+        for i in 0...3 {
+            if reflineColorsCopy.contains(self.colors[i]) && refline.colors[i] != self.colors[i] {
+                result.append(i)
+                reflineColorsCopy.remove(at: reflineColorsCopy.index(of: self.colors[i])!)
+            }
+        }
+        return result
+    }
+
+
+    func result(compareTo refline: Line) -> Array<Int>
+    {
+        let result = [self.wellPlaced(compareTo: refline).count, self.wrongPositions(compareTo: refline).count]
+        return result
+    }
+
 }
--- Sources/swift-mastermind/mastermind.swift
+++ Sources/swift-mastermind/mastermind.swift
@@ -1,10 +1,21 @@
 class Mastermind {
-    var testLine = Line()
+    var refLine = Line()
+    var inputLine = Line.randomLine()
 
     func writeGame() {
-        print(self.testLine)
-        testLine = Line.randomLine()
-        print(self.testLine)
+        
+        refLine = Line.randomLine()
+        print("The reference line is: \(self.refLine)")
+        print("The line inputs by the player is: \(self.inputLine)")
+        print("Let's compare them:")
+        print("inputLine.wellPlaced = \(inputLine.wellPlaced(compareTo: self.refLine))")
+        print("inputLine.wrongPositions = \(inputLine.wrongPositions(compareTo: self.refLine))")
+        
+        print("Result array = \(inputLine.result(compareTo: self.refLine))")
+
+        print("The reference line is: \(self.refLine)")
+
+
     }
     
     
